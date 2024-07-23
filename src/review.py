1c8cd5b9727a7aa2032f16ab6feb281ac9364331
Ângelo Hank <hankangelo6@gmail.com>
2024-07-23 19:15:24 -0300

    fix:  usando snake case e ajustes gerais

--- src/review.py
+++ src/review.py
@@ -3,25 +3,25 @@ import os
 import re
 def review(config):
     merge = config['merge']
-    wrong_pattern = config['wrongPattern']
-    rigth_pattern = config['rigthPattern']
-    regexList = config['regexFile']
+    wrong_pattern = config['wrong_pattern']
+    rigth_pattern = config['rigth_pattern']
+    regex_list = config['regex_list']
     changes = merge['changes']
 
     comments = []
 
-    filesForRegex = []
+    files_for_regex = []
     for change in changes:
-        if re.match(regexList[0], getFileName(change.get('new_path'))):
+        if re.match(regex_list[0], get_file_name(change.get('new_path'))):
             #TODO verificar se o arquivo vai vir no path_source ou new_path
-            filesForRegex.append(change.get('new_path'))
+            files_for_regex.append(change.get('new_path'))
 
-    lines_to_comment = findWrongPattern(filesForRegex, wrong_pattern)
+    lines_to_comment = find_wrong_patterns(files_for_regex, wrong_pattern)
 
-    for key, value in lines_to_comment.items():
+    for line_number, line_data in lines_to_comment.items():
         descr_comment = config['message']
-        descr_comment = descr_comment.replace("${LINE_NUMBER}", str(key))
-        descr_comment = descr_comment.replace("${WRONG_PATTERN}", value.replace("\n", ""))
+        descr_comment = descr_comment.replace("${LINE_NUMBER}", str(line_number))
+        descr_comment = descr_comment.replace("${WRONG_PATTERN}", line_data.replace("\n", ""))
         descr_comment = descr_comment.replace("${RIGHT_PATTERN}", rigth_pattern)
 
         comments.append(commons.comment_create(
@@ -29,25 +29,26 @@ def review(config):
             #TODO pegar o path do arquivo
             comment_path="",
             comment_description=descr_comment,
-            comment_end_line=key,
-            comment_start_line=key,
+            comment_end_line=line_number,
+            comment_start_line=line_data,
         ))
-
+    print(comments)
     return comments
 
-def findWrongPattern(filesForRegex, wrongPattern):
-    defines = {}
+def find_wrong_patterns(files_for_regex, wrong_pattern):
+    wrong_patterns = {}
 
-    for file in filesForRegex:
+    for file in files_for_regex:
         with open(file, 'r', encoding='utf-8') as file:
             lines = file.readlines()
             for line_number, line in enumerate(lines, start=1):
-                if checkWrongPatternExists(line, wrongPattern):
-                    defines[line_number] = line
+                if check_wrong_pattern_exists(line, wrong_pattern):
+                    wrong_patterns[line_number] = line
 
-    return defines
-def getFileName(filePath):
-    return os.path.basename(filePath)
+    return wrong_patterns
 
-def checkWrongPatternExists(line, wrong_pattern):
+def get_file_name(file_path):
+    return os.path.basename(file_path)
+
+def check_wrong_pattern_exists(line, wrong_pattern):
     return line.strip().startswith(wrong_pattern)
