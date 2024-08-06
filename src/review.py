b83580ef6ef0cc7c8c71a38ec5a70ce78ea4612b
Ângelo Hank <hankangelo6@gmail.com>
2024-08-06 13:50:38 -0300

    refactor: transformando wrong_declarations em obj separado

--- src/review.py
+++ src/review.py
@@ -16,33 +16,41 @@ def review(config):
         if re.match(regex_list[0], get_file_name(path_source + '/' + change.get('new_path'))):
             files_for_regex.append(change.get('new_path'))
 
-    lines_to_comment = find_wrong_patterns(files_for_regex, wrong_pattern)
+    #esse cara passa a retornar uma lista de objetos
+    wrong_declations = find_wrong_patterns(files_for_regex, wrong_pattern)
 
-    for line_number, line_data in lines_to_comment.items():
+    for wrong_declaration in wrong_declations:
         descr_comment = config['message']
-        descr_comment = descr_comment.replace("${LINE_NUMBER}", str(line_number))
-        descr_comment = descr_comment.replace("${WRONG_PATTERN}", line_data.replace("\n", ""))
+        descr_comment = descr_comment.replace("${LINE_NUMBER}", str(wrong_declaration['line_number']))
+        descr_comment = descr_comment.replace("${WRONG_PATTERN}", str(wrong_declaration['wrong_pattern']).replace("\n", ""))
         descr_comment = descr_comment.replace("${RIGHT_PATTERN}", rigth_pattern)
 
         comments.append(commons.comment_create(
-            comment_id=commons.comment_generate_id(path_source+str(line_number)),
-            comment_path=path_source,
+            comment_id=commons.comment_generate_id(path_source+str(wrong_declaration['line_number'])),
+            comment_path=wrong_declaration['path'],
             comment_description=descr_comment,
-            comment_end_line=line_number,
-            comment_start_line=line_data,
+            comment_end_line=wrong_declaration['line_number'],
+            comment_start_line=wrong_declaration['line_number'],
         ))
     print(comments)
     return comments
 
 def find_wrong_patterns(files_for_regex, wrong_pattern):
-    wrong_patterns = {}
+    wrong_patterns = []
 
     for file in files_for_regex:
         with open(file, 'r', encoding='utf-8') as file:
             lines = file.readlines()
             for line_number, line in enumerate(lines, start=1):
                 if check_wrong_pattern_exists(line, wrong_pattern):
-                    wrong_patterns[line_number] = line
+
+                    wrong_declaration = {
+                        'line_number': line_number,
+                        'wrong_pattern': line,
+                        'path': file
+                    }
+
+                    wrong_patterns.append(wrong_declaration)
 
     return wrong_patterns
 
